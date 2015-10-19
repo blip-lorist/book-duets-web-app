caaefc424a301216bdabb43fcadef484ea8dbee4
Loraine <loraine@lorainekv.com>
2015-10-19 14:47:30 -0700

    Added some comments so that I know where I'm going with this.

--- app/controllers/book_duets_controller.rb
+++ app/controllers/book_duets_controller.rb
@@ -1,6 +1,13 @@
 class BookDuetsController < ApplicationController
 
   def index
-    
+  end
+
+  def suggested_pairing
+    # Magic happens here to call API and return a suggested pairing
+  end
+
+  def custom_duet
+    # Magic happens here to call API and return a custom duet
   end
 end
