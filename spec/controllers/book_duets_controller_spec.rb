12c8e352d156d98902181d2f9ea8b6b606acdf7c
Loraine <loraine@lorainekv.com>
2015-10-19 16:01:59 -0700

    Corrected vcr setup typo.

--- spec/controllers/book_duets_controller_spec.rb
+++ spec/controllers/book_duets_controller_spec.rb
@@ -11,12 +11,26 @@ RSpec.describe BookDuetsController, type: :controller do
 
   describe "GET #suggested_pairing" do
     it "renders the suggested_pairing template" do
-      get :suggested_pairing
-      expect(response).to render_template("suggested_pairing")
+      VCR.use_cassette 'controllers/suggested_pairing', :record => :once do
+        get :suggested_pairing
+        expect(response).to render_template("suggested_pairing")
+      end
     end
 
-    it "receives a JSON object" do
+    it "authenticates the client and response with success" do
+      VCR.use_cassette 'controllers/suggested_pairing', :record => :once do
+        get :suggested_pairing
+        binding.pry
+        expect(response.response_code).to_not be(401)
+        expect(response.response_code).to be(200)
+      end
+    end
 
+    it "retrieves a JSON object" do
+      VCR.use_cassette 'controllers/suggested_pairing', :record => :once do
+        get :suggested_pairing
+        expect(response.header['Content-Type']).to include 'application/json'
+      end
     end
 
     it "has a JSON response with musician, author, book_duet, and news_source keys" do
