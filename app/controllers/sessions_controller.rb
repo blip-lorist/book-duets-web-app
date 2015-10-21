14c67e9129311dd4acc78835a55e9170812e75f9
Loraine <loraine@lorainekv.com>
2015-10-20 21:00:51 -0700

    skip token verification when creating a new session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
 
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
