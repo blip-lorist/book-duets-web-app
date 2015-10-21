fdd81548d95b4039bc05b6e546594df5036483a8
Loraine <loraine@lorainekv.com>
2015-10-20 19:49:14 -0700

    User model validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates :uid, :provider, :username, presence: true
 
   def self.find_or_create_from_auth_hash(auth_hash)
     user = where(provider: auth_hash[:provider], uid: auth_hash[:uid]).first_or_create
