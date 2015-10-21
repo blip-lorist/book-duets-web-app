7e556fe4c986b246c399585dab5028a5447cca54
Loraine <loraine@lorainekv.com>
2015-10-20 19:46:48 -0700

    User model validation specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(
+    username: "unigoat",
+    uid:      "666",
+    provider: "twitter",
+    profile_image: "unigoat.jpg")
+  }
+
+  describe "validations" do
+    it "is valid" do
+      expect(user).to be_valid
+    end
+
+    it "requires a username" do
+      user.username = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a uid" do
+      user.uid = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a provider" do
+      user.provider = nil
+      expect(user).to be_invalid
+    end
+  end
 end
