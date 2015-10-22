3cc728f379638e7bdcabe7a0115dfeb59c1bd63d
Loraine <loraine@lorainekv.com>
2015-10-21 23:37:31 -0700

    Flash messages for sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,27 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
+  MESSAGES = {
+    auth_failure: "Sorry, your sign-in credentials are invalid!",
+    auth_success: "Sign in successful!",
+    logout_success: "See ya!"
+  }
+
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
+    flash[:success] = MESSAGES[:auth_success]
+    redirect_to root_path
+  end
+
+  def failure
+    flash[:errors] = MESSAGES[:auth_failure]
     redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:success] = MESSAGES[:logout_success]
     redirect_to root_path
   end
 
