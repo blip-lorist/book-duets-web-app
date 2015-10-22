37f4985acf52594e21623a81f5068c14d91f06e4
Loraine <loraine@lorainekv.com>
2015-10-21 19:23:40 -0700

    Error handling and flash messages for missing params / API errors.

--- app/controllers/book_duets_controller.rb
+++ app/controllers/book_duets_controller.rb
@@ -1,5 +1,12 @@
 class BookDuetsController < ApplicationController
 
+  MESSAGES = {
+    create_success: "You've successfully saved a BookDuet.",
+    create_fail: "Sorry, this BookDuet couldn't be saved.",
+    cant_find_author: "This author couldn't be found. Please check the spelling and try again.",
+    cant_find_musician: "This musician couldn't be found. Please check the spelling and try again."
+  }
+
   require "HTTParty"
 
   if Rails.env.production?
@@ -29,9 +36,16 @@ class BookDuetsController < ApplicationController
     author = params[:author]
 
     # Call API and return a custom duet
-
     @custom_duet = HTTParty.get(BASE_URI + "/custom_duet?musician=#{musician}&author=#{author}", :headers => {
       "Book-Duets-Key" => ENV['BOOK_DUETS_API_KEY'] })
+
+    if @custom_duet["error"]
+      if @custom_duet["error"] == "AuthorNotFound"
+        flash[:errors] = MESSAGES[:cant_find_author]
+      elsif @custom_duet["error"] == "LyricsNotFound"
+        flash[:errors] = MESSAGES[:cant_find_musician]
+      end
+    end
   end
 
   def create
@@ -41,8 +55,11 @@ class BookDuetsController < ApplicationController
     duet_text: params[:book_duet],
     user_id: session[:user_id])
 
+    if @book_duet.save
+      flash[:success] = MESSAGES[:create_success]
+    else
+      flash[:errors] = MESSAGES[:create_fail]
+    end
     redirect_to root_path
   end
-
-
 end
