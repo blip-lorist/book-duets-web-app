685fdcf766f03e77a58f772a3bf8394aeb5ad8f7
Loraine <loraine@lorainekv.com>
2015-10-22 22:41:22 -0700

    Removed excess code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(session[:user_id])
-    @book_duets = @user.book_duets
-    @mixtapes = @user.mixtapes
-    # Show last five saved BookDuets
-    # Show mixtapes
   end
 end
