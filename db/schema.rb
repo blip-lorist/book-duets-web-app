beb1b8d1b289f88702ccc1a71688e688b9cbedb5
Loraine <loraine@lorainekv.com>
2015-10-27 10:20:18 -0700

    Added a language filter level column to my book duets table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023044059) do
+ActiveRecord::Schema.define(version: 20151027171825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,10 @@ ActiveRecord::Schema.define(version: 20151023044059) do
     t.string   "musician"
     t.string   "author"
     t.string   "duet_text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "user_id"
+    t.string   "filter_level", default: "filthy"
   end
 
   create_table "book_duets_mixtapes", id: false, force: :cascade do |t|
