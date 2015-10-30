e9a6b42a0bf5a418bc11c2bb5e788a6c152ad252
Loraine <loraine@lorainekv.com>
2015-10-29 17:11:50 -0700

    Created a helper for tweetifying stuff.

--- app/helpers/book_duets_helper.rb
+++ app/helpers/book_duets_helper.rb
@@ -1,2 +1,11 @@
 module BookDuetsHelper
+  def tweetify(musician, author)
+    current_page = request.original_url
+    tweet = "Amazing text mashup of #{musician} and #{author} quotes! #{current_page}"
+    tweet = tweet.gsub(" ", "%20")
+
+    url = "https://twitter.com/intent/tweet?text=" + tweet + "&hashtags=bookduets"
+
+    return url
+  end
 end
