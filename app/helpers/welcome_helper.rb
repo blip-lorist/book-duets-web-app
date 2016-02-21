89105760b0daf57abcade14cb16b95edce70fb22
Loraine <loraine@lorainekv.com>
2016-02-20 22:16:33 -0800

    Decoding form reminder.

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,9 +1,11 @@
+require 'uri'
+
 module WelcomeHelper
     def remember_musician
-        params['musician'] ? params['musician'] : nil 
+        params['musician'] ? URI.decode(params['musician']) : nil
     end
 
     def remember_author
-        params['author'] ? params['author'] : nil
+        params['author'] ? URI.decode(params['author']) : nil
     end
 end
